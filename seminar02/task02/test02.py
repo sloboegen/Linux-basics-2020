316fc13e59359bf577f5d91a11e4a13006ccbc36
sloboegen98 <sloboegen98@ya.ru>
2020-09-13 22:11:18 +0300

    Add task 2

---
+++ seminar02/task02/test02.py
@@ -0,0 +1,20 @@
+import unittest
+import subprocess
+import os
+
+
+class TestSum(unittest.TestCase):
+    def test1(self):
+        p = subprocess.Popen(['sh', 'task02.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        stdout = int(stdout)
+        
+        f = open('numbers.txt', 'r')
+        result = set([line for line in f.readlines()])
+        f.close()
+
+        self.assertEqual(len(result), stdout)
+
+
+if __name__ == '__main__':
+    unittest.main()
