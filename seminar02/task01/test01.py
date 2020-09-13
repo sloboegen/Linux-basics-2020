6a64b633a863ee72f28a123f7925b2865dfc691c
sloboegen98 <sloboegen98@ya.ru>
2020-09-13 21:46:05 +0300

    Add first task

---
+++ seminar02/task01/test01.py
@@ -0,0 +1,24 @@
+import unittest
+import subprocess
+import os
+
+
+class TestSum(unittest.TestCase):
+    def test1(self):
+        p = subprocess.Popen(['sh', 'task01.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        stdout = stdout.decode("utf-8")[:-1]
+
+        result = []
+        with open('table.txt', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                result.append(line.split(' ')[1])
+
+            f.close()
+        
+        self.assertEqual('\n'.join(str(n) for n in result), stdout)
+
+
+if __name__ == '__main__':
+    unittest.main()
