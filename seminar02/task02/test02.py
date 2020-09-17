9b50c10a0bdaa9b6c01c47feb4a20d24c3f4cee0
sloboegen98 <sloboegen98@ya.ru>
2020-09-17 13:26:32 +0300

    Add bash

--- seminar02/task02/test02.py
+++ seminar02/task02/test02.py
@@ -5,7 +5,7 @@ import os
 
 class Test2(unittest.TestCase):
     def test1(self):
-        p = subprocess.Popen(['sh', 'task02.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['bash', 'task02.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
         stdout = int(stdout)
         
