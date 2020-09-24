3a1fc6629e8286e778c795b0490554541e8150e0
Timur Garaev <garaevtimur21@gmail.com>
2020-09-24 21:04:51 +0300

    Fix

--- seminar03/task01/task01.sh
+++ seminar03/task01/task01.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+var1=$RANDOM
+touch rnd.txt
+head -c $var1 /dev/urandom > rnd.txt
+echo $var1
--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-echo 2 24 120
+read line<"numbers.txt"
+out=""
+
+for num in $line; do
+	result=1
+    for (( i = 2; i <= $num; i++ )); do
+     	result=$(($result*$i))
+    done
+    out=$out" "$result
+done
+
+echo $out
