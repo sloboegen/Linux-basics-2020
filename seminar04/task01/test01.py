ff5523172065988ef8fe3b7513ccd5cd33f38f26
sloboegen98 <sloboegen98@ya.ru>
2020-09-29 22:55:06 +0300

    fix test01

--- seminar04/task01/test01.py
+++ seminar04/task01/test01.py
@@ -5,17 +5,25 @@ import os
 
 class Test1(unittest.TestCase):
     def check_stderr(self, command: str, arg: str):
-        p = subprocess.Popen(['make', ,], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['make', arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _, stderr = p.communicate()
+        return stderr is None
+
+    def check_stderr_time(self, program: str):
+         p = subprocess.Popen(['make', 'time', program], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         _, stderr = p.communicate()
         return stderr is None
 
 
     def test1(self):
         self.assertTrue(self.check_stderr('make', 'all'))
-        self.assertTrue(self.check_stderr('make', 'time'))
-        # self.assertTrue(self.check_stderr('make', 'compile'))
-        # self.assertTrue(self.check_stderr('make', 'generate'))
-        # self.assertTrue(self.check_stderr('make', 'clean'))
+        self.assertTrue(self.check_stderr('make', 'build_slow'))
+        self.assertTrue(self.check_stderr('make', 'build_fast'))
+        self.assertTrue(self.check_stderr('make', 'run_slow'))
+        self.assertTrue(self.check_stderr('make', 'run_fast'))
+        self.assertTrue(self.check_stderr_time('fast'))
+        self.assertTrue(self.check_stderr_time('slow'))
+        self.assertTrue(self.check_stderr('make', 'clean'))
 
 
 if __name__ == '__main__':
