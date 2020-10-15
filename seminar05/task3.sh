c739eb77bd2146a3d3fee8ad2e00939ccc3d83e6
Timur Garaev <garaevtimur21@gmail.com>
2020-10-15 21:33:45 +0300

    Task3 ready

--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+# Didn't get the point of task. Here's my representation of it:
+
+path=$1
+
+echo path is $path
+
+a=$(cat $path)
+echo "Commit message is:"
+echo $a
+if [[ "$a" =~ "^issue-[0-9]{1,} *" ]];then
+  echo commit format test passed
+  exit 0
+else
+  echo commit format test failed
+  exit 1
+fi
