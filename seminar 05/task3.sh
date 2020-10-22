5cee32dbf072d92e811b740217062ff235f0ca75
IgorianKiselev02 <51312865+IgorianKiselev02@users.noreply.github.com>
2020-10-22 12:40:24 +0300

    Update task3.sh

--- seminar 05/task3.sh
+++ seminar 05/task3.sh
@@ -1,1 +1,16 @@
-hello
+#!/bin/bash
+
+path=$1
+
+echo path is $path
+
+commit=$(cat $path)
+echo "Commit message is:"
+echo $commit
+if [[ "$commit" =~ "^issue-[0-9]{1,} *" ]];then
+  echo OK
+  exit 0
+else
+  echo fail
+  exit 1
+fi
