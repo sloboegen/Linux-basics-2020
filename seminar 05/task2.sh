a1ea03f7d25d19eef91ec60c576d152abcfafa84
IgorianKiselev02 <51312865+IgorianKiselev02@users.noreply.github.com>
2020-10-22 12:29:36 +0300

    Update task2.sh

--- seminar 05/task2.sh
+++ seminar 05/task2.sh
@@ -1,1 +1,14 @@
-hello
+#!/bin/bash
+git log --pretty=oneline > tmp.txt
+
+echo -n "$word" > "release_notes.md"
+
+while read str; do
+    for word in $str; do
+        if [[ "$word" == "$1" ]]; then
+            echo -n "$word " >> "release_notes.md"
+    done
+    echo  >> "release_notes.md"
+done < "tmp.txt"
+
+rm tmp.txt
