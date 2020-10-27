a58db64c23e749ee58f0e935b4d176d95c1442c1
IgorianKiselev02 <51312865+IgorianKiselev02@users.noreply.github.com>
2020-10-27 18:16:00 +0300

    Add files via upload

---
+++ seminar07/src/server.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = ''
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            conn.sendall("OK:".encode('utf-8')+ data)
